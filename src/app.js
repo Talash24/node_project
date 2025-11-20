eb58970c2a13fde72e814183b157cf270d82f6f2
Talash24 <ashrafuliu2020@gmail.com>
2025-11-20 20:32:53 +0600

    exploring get, post api

--- src/app.js
+++ src/app.js
@@ -2,13 +2,21 @@ const express = require("express");
 
 const app = express();
 
+app.get("/user", (req, res) => {
+    res.send({firstName: "ashraful", lastname: "alam"});
+});
+
 app.use("/hello", (req, res) => {
     res.send("Hello from the server.....");
-})
+});
 
 app.use("/testing", (req, res) => {
     res.send("server testing 1,2,3,4....")
-})
+});
+
+app.post("/user", (req, res) => {
+    res.send("data saved successfully to the DB");
+});
 app.listen(3000, () => {
     console.log("Server is successfully listening on port 3000...");
 });
