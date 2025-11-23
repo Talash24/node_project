d74c4300e103cd6f4c8541790d582f05f973e190
Talash24 <ashrafuliu2020@gmail.com>
2025-11-23 21:12:29 +0600

    User authentication

--- src/app.js
+++ src/app.js
@@ -2,21 +2,56 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
+const {validateSignUpData} = require("./utils/validation");
+const {validateLoginData} = require("./utils/validation");
+const bcrypt = require("bcrypt");
 
 app.use(express.json())
 
 app.post("/signup", async (req, res) => {
-   
-    const user = new User(req.body);
+    
+  try {
 
-  
-   try {
+    const {firstName, lastName, emailId, password} = req.body;
+    const passwordHash = await bcrypt.hash(password, 10)
+
+    validateSignUpData(req);
+    const user = new User({
+        firstName,
+        lastName,
+        emailId,
+        password: passwordHash,
+    });
+
     await user.save();
     res.send("User Added successfully");
    } catch(err) {
     res.status(400).send("Error saving the user: " + err.message);
    }
 });
+
+app.post("/login", async (req, res) => {
+    try{
+        const { emailId, password } = req.body;
+    
+        const user = await User.findOne({ emailId: emailId });
+        if(!user) {
+            throw new Error("EmailId is not present in DB");
+        }
+        validateLoginData(req);
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if(isPasswordValid){
+            res.send("Login Successful!!!");
+        }else{
+            throw new Error("Invalid Email or Password");
+        }
+
+        
+    }catch (err) {
+        res.status(400).send("ERROR : " + err.message);
+
+    }
+})
 
 //find user by email
 app.get("/user", async (req, res) => {
---
+++ src/utils/validation.js
@@ -0,0 +1,23 @@
+const validator = require("validator");
+const validateSignUpData = (req) => {
+    const { firstName, lastName, emailId, password } = req.body;
+    if(!firstName || !lastName) {
+        throw new Error("Name is not valid");
+    }
+    else if(!validator.isEmail(emailId)) {
+        throw new Error("Email is not valid");
+    }
+    else if(!validator.isStrongPassword(password)) {
+        throw new Error("Please enter a strong password");
+    }
+   
+};
+
+const validateLoginData = (req) => {
+    const {emailId} = req.body;
+    if(!validator.isEmail(emailId)){
+        throw new Error("Please provide valid email address");
+    }
+}
+
+module.exports = {validateSignUpData, validateLoginData};
