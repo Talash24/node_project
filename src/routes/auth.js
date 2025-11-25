014476f282fc32972a012316cc0ec77c8bdde80d
Talash24 <ashrafuliu2020@gmail.com>
2025-11-25 23:57:26 +0600

    API to send connection request.

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -50,6 +50,14 @@ router.post("/login", async (req, res) => {
   }
 });
 
+router.post("/logout", async (req, res) => {
+    res.cookie("token", null, {
+        expires: new Date(Date.now()),
+    });
+
+    res.send("Logged Out.");
+});
+
 
 
 
--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const {userAuth} = require("../middlewares/auth")
-router.get("/profile", userAuth,  async (req, res) => {
+const {userAuth} = require("../middlewares/auth");
+const {validateEditProfileData} = require("../utils/validation")
+router.get("/profile/view", userAuth,  async (req, res) => {
   try {
    
     const user = req.user;
@@ -10,5 +11,26 @@ router.get("/profile", userAuth,  async (req, res) => {
     res.status(400).send("ERROR : " + err.message);
   }
 });
+
+router.patch("/profile/edit", userAuth, async (req, res) => {
+    try{
+        if(!validateEditProfileData(req)){
+            throw new Error("Invalid edit request")
+        }
+
+        const loggedInUser = req.user;
+        
+        Object.keys(req.body).forEach((key) => (loggedInUser[key] = req.body[key]));
+
+        await loggedInUser.save();
+        res.json({
+            message: `${loggedInUser.firstName} your Profile Updated Successfully`,
+            data: loggedInUser,
+        });
+
+    }catch(err){
+        res.status(400).send("ERROR : " + err.message);
+    }
+})
 
 module.exports = router;
